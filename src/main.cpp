669326aa61aec5ffcac270cd8d1170741a68135c
Timon Emken <emken@chalmers.se>
2020-05-14 17:49:05 +0200

    Remove relative paths by defining a macro and testing with obscura

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,14 @@
 
 // Headers from libphysica
 #include "Natural_Units.hpp"
+#include "Utilities.hpp"
 
 // Headers from obscura
 #include "Astronomy.hpp"
+#include "Configuration.hpp"
+#include "Target_Nucleus.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
 	//Starting time
 	auto time_start = std::chrono::system_clock::now();
@@ -20,6 +23,14 @@ int main()
 	std::cout <<fib(10) <<std::endl;
 	std::cout <<In_Units(1.0, meter/sec)<<std::endl;
 	std::cout <<Fractional_Days_since_J2000(1,1,2001,12)<<std::endl;
+
+	Import_Nuclear_Data();
+	Configuration cfg(argv[1]);
+	cfg.Print_Summary();
+
+	std::vector<double> DM_masses = Log_Space(cfg.constraints_mass_min, cfg.constraints_mass_max, cfg.constraints_masses);
+	std::vector<std::vector<double>> exclusion_limits = cfg.DM_detector->Upper_Limit_Curve(*(cfg.DM), *(cfg.DM_distr), DM_masses, cfg.constraints_certainty);
+	Export_Table(PROJECT_DIR "results/" + cfg.ID + "/constraints.txt", exclusion_limits,{GeV,cm*cm});
 
 	//Ending time and computing time
 	auto time_end = std::chrono::system_clock::now();
